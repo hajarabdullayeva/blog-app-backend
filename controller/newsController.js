010db2ce262d0997a88b16eea56a5062a1ccb712
HajarAb <hadjar.abdullayeva@gmail.com>
2022-10-12 09:59:03 +0400

    news api

--- controller/newsController.js
+++ controller/newsController.js
@@ -1,9 +1,9 @@
 const News = require("../model/news");
-const { body, validationResult } = require("express-validator");
-const bodyParser = require("body-parser");
+// const { body, validationResult } = require("express-validator");
+// const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 module.exports = {
   getAllNews: async function async(req, res) {
@@ -25,17 +25,17 @@ module.exports = {
   },
 
   getOneNews: function (req, res) {
-    //     try {
-    //       const id = req.params.id;
-    //       const news = News.findById();
-    //       if (!news) return res.send({ success: false, message: "Invalid id " });
-    //       res.json({ success: true, data: { news } });
-    //     } catch (error) {
-    //       res.json({
-    //         success: false,
-    //         message: error,
-    //       });
-    //     }
+    try {
+      const id = req.params.id;
+
+      const news = News.findById(id);
+      console.log(news);
+    } catch (error) {
+      res.json({
+        success: false,
+        message: error,
+      });
+    }
   },
 
   createNews: async function (req, res) {
--- routes/newsRouter.js
+++ routes/newsRouter.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const newsController = require("../controller/newsController");
 
 router.get("/", newsController.getAllNews);
-// router.get("/:id", newsController.getOneNews);
+router.get("/:id", newsController.getOneNews);
 router.post(
   "/",
-  body("title").notEmpty().withMessage("Title is required"),
-  body("content").notEmpty().withMessage("Content  is required"),
+  //   body("title").notEmpty().withMessage("Title is required"),
+  //   body("content").notEmpty().withMessage("Content  is required"),
   newsController.createNews
 );
 
